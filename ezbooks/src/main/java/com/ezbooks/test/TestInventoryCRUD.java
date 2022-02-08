a1ebd4b3952ecdde606fc860456ee21e46d5e246
pramodr1 <pramodvr3940@gmail.com>
2022-02-08 23:44:06 +0530

    move src

---
+++ ezbooks/src/main/java/com/ezbooks/test/TestInventoryCRUD.java
@@ -0,0 +1,13 @@
+package com.ezbooks.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestInventoryCRUD {
+	public static void main(String[] args) {
+		SpringApplication.run(TestInventoryCRUD.class, args);
+	}
+	
+
+}
